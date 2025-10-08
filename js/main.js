edddc02a68f2b838e21e6e1d069ef353512ee386
Andrey Lo <loandrey7@gmail.com>
2025-10-08 13:03:08 +0300

    oculus v1.2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 'use strict'
 
-const tabItem = document.querySelectorAll('.tabs__btn-item'); // поиск в html
-const tabContent = document.querySelectorAll('.tabs__content-item'); // поиск в html
+const tabItem = document.querySelectorAll('.tabs__btn-item'); 
+const tabContent = document.querySelectorAll('.tabs__content-item'); 
 
-tabItem.forEach(function (element) { // события при клике
-	element.addEventListener('click', open); // события при клике
+tabItem.forEach(function (element) { 
+	element.addEventListener('click', open); 
 });
 
 function open(evt) {
-	const tabTarget = evt.currentTarget; // логика
+	const tabTarget = evt.currentTarget; 
 	const button = tabTarget.dataset.button;
 
-	tabItem.forEach(function (item) { // пробегаемся
-		item.classList.remove('tabs__btn-item--active'); // убираем
+	tabItem.forEach(function (item) { 
+		item.classList.remove('tabs__btn-item--active'); 
 	});
 
-	tabContent.forEach(function (item) { // пробегаемся
-		item.classList.remove('tabs__content-item--active'); // убираем
+	tabContent.forEach(function (item) { 
+		item.classList.remove('tabs__content-item--active'); 
 	});
 
-	tabTarget.classList.add('tabs__btn-item--active'); // добавили
+	tabTarget.classList.add('tabs__btn-item--active'); 
 	document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
 
